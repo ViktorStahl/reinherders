b84e8d1a98fd5e4c42e768bffc2cfcd26902a694
Viktor Ståhl <vist8337@student.uu.se>
2018-03-18 18:46:47 +0100

    Added project, hope it works!

---
+++ Reinherders/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour {
+    public InputController InputController;
+    public float cameraSpeed = 1f;
+    public float cameraHeightScale = 10f;
+    public float smoothing = 5f;
+    public float orthoScale = 5f;
+    Vector3 movement;
+    Vector3 zoomOffset;
+    Vector3 camPosition;
+    private void Start()
+    {
+        camPosition = transform.position - transform.forward * (InputController.zoomLevel * cameraHeightScale);
+    }
+
+    private void Update()
+    {
+        movement.Set(InputController.moveCameraHorizontal, 0f, InputController.moveCameraVertical);
+        camPosition += Quaternion.AngleAxis(transform.rotation.eulerAngles[1], Vector3.up) * movement * cameraSpeed;
+        Camera cam = this.GetComponent<Camera>();
+        zoomOffset = transform.forward * (InputController.zoomLevel * cameraHeightScale);
+        transform.position = Vector3.Lerp(transform.position, camPosition - zoomOffset, smoothing * Time.deltaTime);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, InputController.zoomLevel * orthoScale, smoothing * Time.deltaTime);
+    }
+}
---
+++ Reinherders/Assets/Scripts/InputController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+    public float moveCameraHorizontal = 0f;
+    public float moveCameraVertical = 0f;
+    public float zoomLevel = 1f;
+
+    private float borderTreshold = 0.1f;
+
+    void Update() {
+        //Debug.Log("Debug: " + "Hello world");
+        UpdateHorizontal();
+        UpdateVertical();
+        UpdateZoom();
+    }
+
+    private void UpdateZoom()
+    {
+        zoomLevel -= Input.GetAxisRaw("Mouse ScrollWheel");
+        if (zoomLevel < 0.1f) zoomLevel = 0.1f;
+        if (zoomLevel > 4f) zoomLevel = 4f;
+    }
+
+    private void UpdateHorizontal()
+    {
+        float mouseX = Input.mousePosition.x;
+        float borderLeft = (float)Screen.width * borderTreshold;
+        float borderRight = (float)Screen.width - (Screen.width * borderTreshold);
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            moveCameraHorizontal = Input.GetAxisRaw("Horizontal");
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            moveCameraHorizontal = Input.GetAxisRaw("Horizontal");
+        }
+        else if (mouseX < borderLeft && mouseX >= 0)
+        {
+            moveCameraHorizontal = (1 - mouseX / borderLeft) * -1;
+        }
+        else if (mouseX > borderRight && mouseX <= Screen.width)
+        {
+            moveCameraHorizontal = (mouseX - borderRight) / (Screen.width * borderTreshold) * 1;
+        }
+        else
+        {
+            moveCameraHorizontal = 0f;
+        }
+    }
+
+    private void UpdateVertical()
+    {
+        float mouseY = Input.mousePosition.y;
+        float borderBottom = (float)Screen.height * borderTreshold;
+        float borderTop = (float)Screen.height - (Screen.height * borderTreshold);
+        if (Input.GetAxisRaw("Vertical") > 0f)
+        {
+            moveCameraVertical = Input.GetAxisRaw("Vertical");
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0f)
+        {
+            moveCameraVertical = Input.GetAxisRaw("Vertical");
+        }
+        else if (mouseY < borderBottom && mouseY >= 0)
+        {
+            moveCameraVertical = (1 - mouseY / borderBottom) * -1;
+        }
+        else if (mouseY > borderTop && mouseY <= Screen.height)
+        {
+            moveCameraVertical = (mouseY - borderTop) / (Screen.height * borderTreshold) * 1;
+        }
+        else
+        {
+            moveCameraVertical = 0f;
+        }
+    }
+}
